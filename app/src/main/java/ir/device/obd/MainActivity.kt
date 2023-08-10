b227b53d5727833350c10e5b46c90409ea2e8a7b
Mohammadreza <m.r.1378.m.r@gmail.com>
2023-08-10 16:24:54 +0330

    adding some function about connecting  to bluetooth device

--- app/src/main/java/ir/device/obd/MainActivity.kt
+++ app/src/main/java/ir/device/obd/MainActivity.kt
@@ -31,6 +31,9 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import org.w3c.dom.Text
 import java.io.IOException
 import java.util.UUID
@@ -41,73 +44,85 @@ class MainActivity : AppCompatActivity() {
     private lateinit var context: Context
 
 
-
     private val PERMISSION_REQUEST_BLU = 102
 
     lateinit var bluetoothManager: BluetoothManager
     lateinit var bluetoothAdapter: BluetoothAdapter
-    lateinit var receiver:BluetoothReceiver
-    lateinit var receiver2:Discoverability
-    private lateinit var btOnOff:Button
-    private lateinit var btDiscoverability:Button
-    private lateinit var btGetPairedDevices:Button
-    private lateinit var btDiscoverDevices:Button
+    lateinit var receiver: BluetoothReceiver
+    lateinit var receiver2: Discoverability
+    private lateinit var btOnOff: Button
+    private lateinit var btDiscoverability: Button
+    private lateinit var btGetPairedDevices: Button
+    private lateinit var btDiscoverDevices: Button
 
-    private lateinit var device1:TextView
-    private lateinit var device2:TextView
-    private lateinit var device3:TextView
+    private lateinit var device1: TextView
+    private lateinit var device2: TextView
+    private lateinit var device3: TextView
 
-    private lateinit var foundedDevice:BluetoothDevice
+    private lateinit var foundedDevice: BluetoothDevice
 
-    val REQUEST_ACCESS_COARSE_LOCATION=101
+    val REQUEST_ACCESS_COARSE_LOCATION = 101
 
     @SuppressLint("MissingPermission")
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btOnOff=findViewById(R.id.btOnOff)
-        btDiscoverability=findViewById(R.id.btDiscoverability)
-        btGetPairedDevices=findViewById(R.id.btGetPairedDevices)
-        btDiscoverDevices=findViewById(R.id.btDiscoverDevices)
+        btOnOff = findViewById(R.id.btOnOff)
+        btDiscoverability = findViewById(R.id.btDiscoverability)
+        btGetPairedDevices = findViewById(R.id.btGetPairedDevices)
+        btDiscoverDevices = findViewById(R.id.btDiscoverDevices)
 
-        device1=findViewById(R.id.device1)
-        device2=findViewById(R.id.device2)
-        device3=findViewById(R.id.device3)
+        device1 = findViewById(R.id.device1)
+        device2 = findViewById(R.id.device2)
+        device3 = findViewById(R.id.device3)
 
 
 
         requestPermission()
 
-        bluetoothManager=getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothAdapter=bluetoothManager.adapter
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = bluetoothManager.adapter
         enableDisableBT()
         enableDisableDiscoverability()
-        receiver=BluetoothReceiver()
-        receiver2= Discoverability()
+        receiver = BluetoothReceiver()
+        receiver2 = Discoverability()
 //        btOnOff.setOnClickListener{
 //
 //        }
-        btGetPairedDevices.setOnClickListener{
+        btGetPairedDevices.setOnClickListener {
             getPairedDevices()
         }
 
-        btDiscoverDevices.setOnClickListener{
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-                when(ContextCompat.checkSelfPermission(baseContext,Manifest.permission.ACCESS_COARSE_LOCATION)){
-                    PackageManager.PERMISSION_DENIED->androidx.appcompat.app.AlertDialog.Builder(this)
+        btDiscoverDevices.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                when (ContextCompat.checkSelfPermission(
+                    baseContext,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )) {
+                    PackageManager.PERMISSION_DENIED -> androidx.appcompat.app.AlertDialog.Builder(
+                        this
+                    )
                         .setTitle("RunTime vPermision")
                         .setMessage("Give Permision")
-                        .setNeutralButton("okay",DialogInterface.OnClickListener{dialog,which->
-                            if(ContextCompat.checkSelfPermission(baseContext,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-                                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),REQUEST_ACCESS_COARSE_LOCATION)
+                        .setNeutralButton("okay", DialogInterface.OnClickListener { dialog, which ->
+                            if (ContextCompat.checkSelfPermission(
+                                    baseContext,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION
+                                ) != PackageManager.PERMISSION_GRANTED
+                            ) {
+                                ActivityCompat.requestPermissions(
+                                    this,
+                                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                                    REQUEST_ACCESS_COARSE_LOCATION
+                                )
                             }
                         }).show()
 //                        .findViewById<TextView>(R.id.message)!!.movementMethod=LinkMovementMethod.getInstance()
 
 
-                    PackageManager.PERMISSION_GRANTED->{
-                        Log.d("message1","permission granted")
+                    PackageManager.PERMISSION_GRANTED -> {
+                        Log.d("message1", "permission granted")
                     }
                 }
             }
@@ -116,9 +131,6 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-
-
-
 
 
     private fun requestPermission() {
@@ -130,6 +142,7 @@ class MainActivity : AppCompatActivity() {
             ), PERMISSION_REQUEST_ACCESS_LOCATION
         )
     }
+
     companion object {
         private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
     }
@@ -137,45 +150,48 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     private fun discoverDevices() {
-        Log.e("message1","discover device")
-       val filter=IntentFilter()
+        Log.e("message1", "discover device")
+        val filter = IntentFilter()
         filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
         filter.addAction(BluetoothDevice.ACTION_FOUND)
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
 
-        registerReceiver(discoverDeviceReceiver,filter)
+        registerReceiver(discoverDeviceReceiver, filter)
         bluetoothAdapter.startDiscovery()
-        Log.e("message1",bluetoothAdapter.startDiscovery().toString())
+        Log.e("message1", bluetoothAdapter.startDiscovery().toString())
 
     }
 
-    var it=0
-    private val discoverDeviceReceiver=object :BroadcastReceiver(){
+    var it = 0
+    private val discoverDeviceReceiver = object : BroadcastReceiver() {
 
         @SuppressLint("MissingPermission")
         override fun onReceive(context: Context?, intent: Intent?) {
 
-            var action=""
-            if(intent!=null){
-                 action= intent.action.toString()
+            var action = ""
+            if (intent != null) {
+                action = intent.action.toString()
             }
-            when(action){
-                BluetoothAdapter.ACTION_STATE_CHANGED->{
-                    Log.d("message1","States Changed")
+            when (action) {
+                BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                    Log.d("message1", "States Changed")
                 }
-                BluetoothAdapter.ACTION_DISCOVERY_STARTED->{
-                    Log.d("message1","discovery start ")
+
+                BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
+                    Log.d("message1", "discovery start ")
                 }
-                BluetoothAdapter.ACTION_DISCOVERY_FINISHED->{
-                    Log.d("message1","discovery finished ")
+
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+                    Log.d("message1", "discovery finished ")
 
                 }
-                BluetoothDevice.ACTION_FOUND ->{
-                    val device:BluetoothDevice =
+
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device: BluetoothDevice =
                         intent!!.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
-                    if(device!=null){
-                        Log.d("message1","${device.name} ${device.address} ${device.uuids}")
+                    if (device != null) {
+                        Log.d("message1", "${device.name} ${device.address} ${device.uuids}")
                         it++
 //                        when(it){
 //                            1->{
@@ -197,25 +213,27 @@ class MainActivity : AppCompatActivity() {
 //                                }
 //                            }
 //                        }
-                        if(!device.name.isNullOrEmpty()){
-                            device1.text=device.name
-                            foundedDevice=device
+                        if (!device.name.isNullOrEmpty()) {
+                            device1.text = device.name
+                            foundedDevice = device
 //                            device.createRfcommSocketToServiceRecord(UUID.fromString(device.uuids.toString()))
 
-                            if(device.name=="YSW10"){
+                            if (device.name == "YSW10") {
 //                                AcceptThread().run()
 //                                ConnectThread(device).run()
                             }
                         }
-                        when(device.bondState){
-                            BluetoothDevice.BOND_NONE->{
-                                Log.d("message1","${device.name} bond none")
+                        when (device.bondState) {
+                            BluetoothDevice.BOND_NONE -> {
+                                Log.d("message1", "${device.name} bond none")
                             }
-                            BluetoothDevice.BOND_BONDING->{
-                                Log.d("message1","${device.name} bond BONDING")
+
+                            BluetoothDevice.BOND_BONDING -> {
+                                Log.d("message1", "${device.name} bond BONDING")
                             }
-                            BluetoothDevice.BOND_BONDED->{
-                                Log.d("message1","${device.name} bond BONDED")
+
+                            BluetoothDevice.BOND_BONDED -> {
+                                Log.d("message1", "${device.name} bond BONDED")
                             }
                         }
 
@@ -228,58 +246,74 @@ class MainActivity : AppCompatActivity() {
 
 
     @SuppressLint("MissingPermission")
-    private fun getPairedDevices(){
-       var arr= bluetoothAdapter.bondedDevices
-        Log.d("message1",arr.size.toString())
-        Log.d("message1",arr.toString())
-        for(device in arr){
-            Log.d("message1",device.name+ " " +device.address +" " + device.uuids)
+    private fun getPairedDevices() {
+        var arr = bluetoothAdapter.bondedDevices
+        Log.d("message1", arr.size.toString())
+        Log.d("message1", arr.toString())
+        for (device in arr) {
+            Log.d("message1", device.name + " " + device.address + " " + device.uuids)
         }
     }
 
     private fun enableDisableDiscoverability() {
-        when{
-            ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE)==PackageManager.PERMISSION_GRANTED->{
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_ADVERTISE
+            ) == PackageManager.PERMISSION_GRANTED -> {
 
             }
-            shouldShowRequestPermissionRationale(Manifest.permission.BLUETOOTH_ADVERTISE)->{
-                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.BLUETOOTH_ADVERTISE),101)
+
+            shouldShowRequestPermissionRationale(Manifest.permission.BLUETOOTH_ADVERTISE) -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.BLUETOOTH_ADVERTISE),
+                    101
+                )
             }
         }
-        btDiscoverability.setOnClickListener{
-            val discoverIntent=Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
-            discoverIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,20)
+        btDiscoverability.setOnClickListener {
+            val discoverIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+            discoverIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 20)
             startActivity(discoverIntent)
 
-            val intentFilter=IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)
-            registerReceiver(receiver2,intentFilter)
+            val intentFilter = IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)
+            registerReceiver(receiver2, intentFilter)
         }
     }
 
     private fun enableDisableBT() {
-        Log.d("message1","enableBt")
-        when{
-            ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)==PackageManager.PERMISSION_GRANTED->{
+        Log.d("message1", "enableBt")
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED -> {
 
             }
-            shouldShowRequestPermissionRationale(Manifest.permission.BLUETOOTH_CONNECT)->{
-                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.BLUETOOTH_CONNECT),101)
+
+            shouldShowRequestPermissionRationale(Manifest.permission.BLUETOOTH_CONNECT) -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                    101
+                )
             }
         }
-        btOnOff.setOnClickListener{
-            if(!bluetoothAdapter.isEnabled){
+        btOnOff.setOnClickListener {
+            if (!bluetoothAdapter.isEnabled) {
                 bluetoothAdapter.enable()
-                val intent =Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivities(arrayOf(intent))
 
-                val intentFilter=IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
-                registerReceiver(receiver,intentFilter)
+                val intentFilter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+                registerReceiver(receiver, intentFilter)
             }
-            if(bluetoothAdapter.isEnabled){
+            if (bluetoothAdapter.isEnabled) {
                 bluetoothAdapter.disable()
 
-                val intentFilter=IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
-                registerReceiver(receiver,intentFilter)
+                val intentFilter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+                registerReceiver(receiver, intentFilter)
             }
         }
     }
@@ -291,12 +325,14 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     @SuppressLint("MissingPermission")
     private inner class AcceptThread : Thread() {
 
         private val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
-            bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord("OBD", UUID.fromString("6680b2c1-bd6f-41b0-b3be-023ca4cb4fd3"))
+            bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord(
+                "OBD",
+                UUID.fromString("6680b2c1-bd6f-41b0-b3be-023ca4cb4fd3")
+            )
         }
 
         override fun run() {
@@ -363,5 +399,33 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    sealed class ConnectionState {
+        class Connecting(val bluetoothDevice: BluetoothDevice) : ConnectionState()
+        class Connected(val socket: BluetoothSocket) : ConnectionState()
+        class ConnectionFailed(val failureReason: String) : ConnectionState()
+        object Disconnected : ConnectionState()
+    }
+
+    // This is the standard UUID which can be used for all kind of device connection
+    private val STANDARD_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+    private lateinit var socket:BluetoothSocket
+    private lateinit var connectedDevice:BluetoothDevice
+    @SuppressLint("MissingPermission")
+    fun connectToDevice(bluetoothDevice: BluetoothDevice) = flow {
+        emit(ConnectionState.Connecting(bluetoothDevice))
+        bluetoothAdapter.cancelDiscovery()
+        try {
+        socket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(STANDARD_UUID)
+            ?.also { it.connect() }!!
+            connectedDevice = bluetoothDevice
+            socket.let {
+            emit(
+                ConnectionState.Connected(it)
+            )
+        }
+    } catch (e: Exception) {
+        emit(ConnectionState.ConnectionFailed(e.message ?: "Failed to connect"))
+    }
+    }.flowOn(Dispatchers.IO)
 
 }
